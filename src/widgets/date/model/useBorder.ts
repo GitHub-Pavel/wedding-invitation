c9f7c5532d30965585c64551826586b736264ca0
Pavel K. <pkhatkevich@gmail.com>
2025-07-25 13:11:37 +0300

    fix

--- src/widgets/date/model/useBorder.ts
+++ src/widgets/date/model/useBorder.ts
@@ -1,11 +1,16 @@
 "use client";
 
 import { AnimationScope } from "motion";
+import { RefObject, useMemo, useRef } from "react";
 import { PropsWithScopes } from "@/shared/types";
-import { cutPercentagesAfter, cutPercentagesBefore } from "@/shared/utils/math";
 import { useThrottleCallback } from "@react-hook/throttle";
-import { AnimationOptions, useAnimate, useInView } from "motion/react";
-import { RefObject, useCallback, useEffect, useMemo, useRef } from "react";
+import {
+  AnimationOptions,
+  useAnimate,
+  useAnimationFrame,
+  useInView,
+} from "motion/react";
+import { cutPercentagesAfter, cutPercentagesBefore } from "@/shared/utils/math";
 
 export interface BorderScopes {
   scope1?: AnimationScope<HTMLDivElement>;
@@ -16,89 +21,83 @@ export interface BorderScopes {
   scope6?: AnimationScope<HTMLDivElement>;
 }
 
+interface BorderParams {
+  fps: number;
+  duration: number;
+}
+
 type BorderReturn = PropsWithScopes<BorderScopes>;
 
 const animOpts: AnimationOptions = {
-  duration: 0.01,
+  duration: 0,
   ease: "linear",
 };
 
+const initParams: BorderParams = {
+  fps: 240,
+  duration: 200,
+};
+
 export const useBorder = (
-  wrap: RefObject<HTMLElement | null>
+  wrap: RefObject<HTMLElement | null>,
+  params?: Partial<BorderParams>
 ): BorderReturn => {
-  const lastScrollTop = useRef(0);
-  const isInView = useInView(wrap);
+  const opts = { ...initParams, ...params };
   const [scope1, animate1] = useAnimate();
   const [scope2, animate2] = useAnimate();
   const [scope3, animate3] = useAnimate();
   const [scope4, animate4] = useAnimate();
   const [scope5, animate5] = useAnimate();
   const [scope6, animate6] = useAnimate();
+  const isInView = useInView(wrap, {
+    margin: "-400px 0px -400px",
+  });
+  const percentages = useRef(0);
 
-  const handleDownAnimate = useThrottleCallback(
-    async (percentages: number, downScroll: boolean) => {
-      const topOffset = 8;
-      const topPer = cutPercentagesAfter(percentages, topOffset);
-      const topWidth = `${topPer / 2}%`;
-      const sideOffset = 55;
-      const sidePer = cutPercentagesAfter(
-        cutPercentagesBefore(percentages, topOffset),
-        sideOffset
-      );
-      const height = `${sidePer}%`;
-      const botPer = cutPercentagesAfter(
-        cutPercentagesBefore(
-          cutPercentagesBefore(percentages, topOffset),
-          sideOffset
-        ),
-        70
-      );
-      const botWidth = `${botPer / 2}%`;
-
-      if (downScroll) {
-        animate1(scope1.current, { width: topWidth }, animOpts);
-        await animate2(scope2.current, { width: topWidth }, animOpts);
-
-        animate3(scope3.current, { height }, animOpts);
-        await animate4(scope4.current, { height }, animOpts);
-
-        animate5(scope5.current, { width: botWidth }, animOpts);
-        animate6(scope6.current, { width: botWidth }, animOpts);
-      } else {
-        animate5(scope5.current, { width: botWidth }, animOpts);
-        await animate6(scope6.current, { width: botWidth }, animOpts);
-
-        animate3(scope3.current, { height }, animOpts);
-        await animate4(scope4.current, { height }, animOpts);
-
-        animate1(scope1.current, { width: topWidth }, animOpts);
-        animate2(scope2.current, { width: topWidth }, animOpts);
-      }
-    },
-    240
-  );
-
-  const handleScroll = useCallback(() => {
-    if (!isInView || !wrap.current) return;
-    const currentScroll =
-      window.pageYOffset || document.documentElement.scrollTop;
-    const scrollY = Math.max(
-      window.scrollY + window.innerHeight / 2 - wrap.current.offsetTop,
-      0
+  const animateBorder = useThrottleCallback(async () => {
+    const topOffset = 20;
+    const sideOffset = 70;
+    const topPer = cutPercentagesAfter(percentages.current, topOffset);
+    const sidePer = cutPercentagesAfter(
+      cutPercentagesBefore(percentages.current, topOffset),
+      sideOffset
     );
-    handleDownAnimate(
-      Math.min(scrollY / (wrap.current.offsetHeight / 100), 100),
-      currentScroll > lastScrollTop.current
+    const botPer = cutPercentagesBefore(
+      cutPercentagesBefore(percentages.current, topOffset),
+      sideOffset
     );
-    lastScrollTop.current = currentScroll <= 0 ? 0 : currentScroll;
-  }, [handleDownAnimate, isInView, wrap]);
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [handleScroll]);
+    const topWidth = `${topPer / 2}%`;
+    const height = `${sidePer}%`;
+    const botWidth = `${botPer / 2}%`;
+
+    if (isInView) {
+      animate1(scope1.current, { width: topWidth }, animOpts);
+      await animate2(scope2.current, { width: topWidth }, animOpts);
+
+      animate3(scope3.current, { height }, animOpts);
+      await animate4(scope4.current, { height }, animOpts);
+
+      animate5(scope5.current, { width: botWidth }, animOpts);
+      animate6(scope6.current, { width: botWidth }, animOpts);
+    } else {
+      animate5(scope5.current, { width: botWidth }, animOpts);
+      await animate6(scope6.current, { width: botWidth }, animOpts);
+
+      animate3(scope3.current, { height }, animOpts);
+      await animate4(scope4.current, { height }, animOpts);
+
+      animate1(scope1.current, { width: topWidth }, animOpts);
+      animate2(scope2.current, { width: topWidth }, animOpts);
+    }
+  }, opts.fps);
+
+  useAnimationFrame((time) => {
+    const axios = [-1, 1][+isInView];
+    const per = percentages.current + (time / (opts.duration * 100)) * axios;
+    percentages.current = Math.max(Math.min(per, 100), 0);
+    animateBorder();
+  });
 
   return useMemo(
     () => ({
